2e42f94ed3f0fbbd647431326b6681a6ea3384aa
bioqu <61717138+bioqu@users.noreply.github.com>
2024-05-25 16:56:20 -0400

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import TrackRepair from './components/TrackRepair';
+
+const App = () => {
+  return (
+    <div className="App">
+      <TrackRepair />
+    </div>
+  );
+};
+
+export default App;
